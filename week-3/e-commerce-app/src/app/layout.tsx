06842fb656856772f1e44847001c337252aa8c00
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-09 19:20:59 +0300

    fix:typo

--- week-3/e-commerce-app/src/app/layout.tsx
+++ week-3/e-commerce-app/src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
                   <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
                 </li> */}
               <li>
-                <Link href="/card">
+                <Link href="/cart">
                   <div className="relative inline-flex items-center p-1.5 text-sm font-medium text-center text-gray-900 bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-300 dark:hover:bg-gray-400 dark:focus:ring-blue-800">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
