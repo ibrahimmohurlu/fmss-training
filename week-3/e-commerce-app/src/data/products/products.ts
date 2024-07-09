f4fb8ae377a5394d0f3fcd8a5e8d6ed921f598b3
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-07-09 22:20:53 +0300

    fetchByProductId method implemented.

--- week-3/e-commerce-app/src/data/products/products.ts
+++ week-3/e-commerce-app/src/data/products/products.ts
@@ -5,3 +5,9 @@ export async function fetchProducts(): Promise<Product[]> {
     const { products } = await res.json() as ProductsResponse;
     return products;
 }
+
+export async function fetchProductById(id: string): Promise<Product> {
+    const res = await fetch(`https://dummyjson.com/products/${id}`);
+    const product = await res.json() as Product;
+    return product;
+}
